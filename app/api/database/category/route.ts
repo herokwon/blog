f658b325807eb01ddf81f4bf6f637ee554ee1dd2
herokwon <wooyng825@naver.com>
2023-12-13 16:21:16 +0900

    Feat (api/notion) : Category, Tag Data Fetch API 추가

---
+++ app/api/database/category/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { getHeaders, notionDatabaseUrl } from "@/app/data/notion";
+import { DatabaseObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export const GET = async () => {
+    try {
+        const response = await fetch(notionDatabaseUrl, {
+            method: "GET",
+            headers: getHeaders,
+            next: { revalidate: 0 }
+        });
+
+        if (!response.ok) throw new Error(response.statusText);
+
+        const responseData: DatabaseObjectResponse = await response.json();
+        const categoryProperty = responseData.properties.Category;
+
+        if (categoryProperty.type !== "select") throw new Error("Wrong Access");
+
+        const categories = categoryProperty.select.options.map(({ name }) => ({ name }));
+        return NextResponse.json({ categories: categories });
+    } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        return NextResponse.json({ status: 500, statusText: message });
+    }
+};
