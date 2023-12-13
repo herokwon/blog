432977e8525c5d773a9b7fffe87e9344f5056edc
herokwon <wooyng825@naver.com>
2023-12-13 16:58:18 +0900

    Feat (api/notion) : Notion Block/Blocks Fetch API 추가

--- app/types/notion.ts
+++ app/types/notion.ts
@@ -1,28 +1,28 @@
-import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { BlockObjectResponse, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export class ArticlesResponse {
     constructor(
-        private _articles: PageObjectResponse[], 
+        private _articles: PageObjectResponse[],
         private _nextCursor: string | null
-    ) {};
-    
-    get articles(): PageObjectResponse[] { 
+    ) { };
+
+    get articles(): PageObjectResponse[] {
         return this._articles;
     }
-    get nextCursor(): string | null { 
+    get nextCursor(): string | null {
         return this._nextCursor;
     }
 };
 
 export class SummaryResponse {
     constructor(
-        private _summaryPieces: string[], 
+        private _summaryPieces: string[],
         private _summaryLength: number,
         private _nextCursor: string | null,
-    ) {};
-    
-    get summaryPieces(): string[] { 
-        return this._summaryPieces; 
+    ) { };
+
+    get summaryPieces(): string[] {
+        return this._summaryPieces;
     }
     get summaryLength(): number {
         return this._summaryLength;
@@ -37,6 +37,24 @@ export class SummaryResponse {
     set summaryLength(length: number) {
         this._summaryLength = length;
     }
+    set nextCursor(cursor: string | null) {
+        this._nextCursor = cursor;
+    }
+};
+
+export class BlocksResponse {
+    constructor(
+        private _blocks: BlockObjectResponse[],
+        private _nextCursor: string | null,
+    ) { };
+
+    get blocks(): BlockObjectResponse[] {
+        return this._blocks;
+    }
+    get nextCursor(): string | null {
+        return this._nextCursor;
+    }
+
     set nextCursor(cursor: string | null) {
         this._nextCursor = cursor;
     }
