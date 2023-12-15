baeb1f3842a74c2c9854bc3acc83c803b48c41d6
herokwon <wooyng825@naver.com>
2023-12-15 16:39:28 +0900

    Chore: Next.js 환경설정 업데이트 (외부 이미지 경로 추가)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "prod-files-secure.s3.us-west-2.amazonaws.com"
+            }
+        ],
+    },
     experimental: {
         turbo: {
             rules: {
                 '*.svg': {
                     loaders: ['@svgr/webpack'],
                     as: '*.js',
-                  },
+                },
             }
         }
     }
