108b004c3dfc987212cdfc6af5f101069efa61c3
herokwon <wooyng825@naver.com>
2023-12-17 17:15:29 +0900

    Feat (page-header) : PageHeader Component 생성

--- app/posts/[category]/layout.tsx
+++ app/posts/[category]/layout.tsx
@@ -1,5 +1,9 @@
 import { baseUrl } from "@/app/lib/data/api";
+import { ArticleCategory } from "@/app/lib/data/notion";
 import { CategoryResponse } from "@/app/types/notion";
+import PageHeader from "@/app/components/PageHeader";
+import Image from "next/image";
+import { getTheme } from "@/app/lib/functions/theme";
 
 export const generateStaticParams = async () => {
     const response = await fetch(`${baseUrl}/api/database/category`);
@@ -8,9 +12,12 @@ export const generateStaticParams = async () => {
     return categories;
 };
 
-export default function CategoryLayout({ children }: { children: React.ReactNode }) {
+export default function CategoryLayout({ params, children }: { params: { category: string }; children: React.ReactNode }) {
     return (
         <main className="main-wrapper">
+            <PageHeader title={ArticleCategory[params.category]}>
+                <Image src={`/images/${params.category}.png`} fill sizes="1x" className="object-cover object-center opacity-25" alt="header-image" />
+            </PageHeader>
             {children}
         </main>
     );
