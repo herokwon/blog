b4449e83248a35ece881ad0506a7168996ef54e6
herokwon <wooyng825@naver.com>
2023-12-19 00:00:22 +0900

    Feat (sidebar) : Sidebar 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import "./globals.css";
 import { Noto_Sans_KR } from "next/font/google";
 import { getTheme, updateTheme } from "./lib/functions/theme";
+import { baseUrl } from "./lib/data/api";
 import Nav from "./components/Nav";
+import Sidebar from "./components/Sidebar";
+import { ArticleResponse } from "./types/notion";
 
 const noto_sans_kr = Noto_Sans_KR({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -13,12 +16,19 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     const savedTheme = await getTheme();
     if (!savedTheme) updateTheme("light");
 
+    const response = await fetch(`${baseUrl}/api/database/article?page_size=3`);
+
+    const articleResponse: ArticleResponse = await response.json();
+
+    // const articleData = new ArticleResponse(articleResponse.items, null);
+
     return (
         <html lang="ko" className={savedTheme ?? "light"}>
             <head>
             </head>
             <body className={noto_sans_kr.variable}>
                 <Nav initTheme={savedTheme ?? "light"} />
+                <Sidebar latestArticles={articleResponse.items} />
                 {children}
             </body>
         </html>
