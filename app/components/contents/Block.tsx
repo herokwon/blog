298ded82e7bc27eefed6bd2937777ba01188838f
herokwon <wooyng825@naver.com>
2023-12-22 07:29:07 +0900

    Feat (article-contents) : Article Header & Content 영역 업데이트

---
+++ app/components/contents/Block.tsx
@@ -0,0 +1,78 @@
+import { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+interface BlockProps {
+    block: BlockObjectResponse;
+    blocks: BlockObjectResponse[];
+    index: number;
+};
+
+export default async function Block({ block, blocks, index }: BlockProps) {
+    switch (block.type) {
+        case "audio":
+            return null;
+        case "bookmark":
+            return null;
+        case "breadcrumb":
+            return null;
+        case "bulleted_list_item":
+            return null;
+        case "callout":
+            return null;
+        case "child_database":
+            return null;
+        case "child_page":
+            return null;
+        case "code":
+            return null;
+        case "column":
+            return null;
+        case "column_list":
+            return null;
+        case "divider":
+            return null;
+        case "embed":
+            return null;
+        case "equation":
+            return null;
+        case "file":
+            return null;
+        case "heading_1":
+            return null;
+        case "heading_2":
+            return null;
+        case "heading_3":
+            return null;
+        case "image":
+            return null;
+        case "link_preview":
+            return null;
+        case "link_to_page":
+            return null;
+        case "numbered_list_item":
+            return null;
+        case "paragraph":
+            return null;
+        case "pdf":
+            return null;
+        case "quote":
+            return null;
+        case "synced_block":
+            return null;
+        case "table":
+            return null;
+        case "table_of_contents":
+            return null;
+        case "table_row":
+            return null;
+        case "template":
+            return null;
+        case "to_do":
+            return null;
+        case "toggle":
+            return null;
+        case "unsupported":
+            return null;
+        case "video":
+            return null;
+    }
+}
--- app/posts/[category]/[title]/layout.tsx
+++ app/posts/[category]/[title]/layout.tsx
@@ -1,9 +1,10 @@
-import PageHeader from "@/app/components/PageHeader";
-import CategoryButton from "@/app/components/articles/CategoryButton";
 import { notFound } from "next/navigation";
 import { baseUrl } from "@/app/lib/data/api";
 import { extractArticleProperties } from "@/app/lib/functions/notion";
 import { ArticleCategory, ArticleResponse } from "@/app/types/notion";
+import { getDate } from "@/app/lib/utils/getDate";
+import PageHeader from "@/app/components/PageHeader";
+import CategoryButton from "@/app/components/articles/CategoryButton";
 
 interface ContentLayoutProps {
     params: {
@@ -33,7 +34,8 @@ export default async function ContentLayout({ params, children }: ContentLayoutP
             <PageHeader imageUrl={properties.Thumbnail.url} title={properties.Title ?? decodeURIComponent(params.title)}>
                 <div className="w-full my-4 flex flex-col justify-center items-center absolute bottom-0 left-0 z-[1]">
                     <CategoryButton category={params.category} />
-                    <h1 className="text-center text-dark">{decodeURIComponent(params.title)}</h1>
+                    <h1 className="py-4 text-center text-dark">{decodeURIComponent(params.title)}</h1>
+                    <span className="text-[0.72rem]">{getDate(properties.Date)}</span>
                 </div>
             </PageHeader>
             {children}
