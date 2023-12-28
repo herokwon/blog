751f5191d46838156a343a8cebaf42b00b82e694
herokwon <wooyng825@naver.com>
2023-12-28 15:56:29 +0900

    Add (database-type) : Update ArticleSubProperty

--- app/posts/[category]/page.tsx
+++ app/posts/[category]/page.tsx
@@ -1,3 +1,5 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
 import { ArticleCategory, ArticleCategoryKeywords, ArticleResponse } from "@/app/types/notion";
 import { fetchArticle } from "@/app/lib/databases";
 import { extractArticleProperties } from "@/app/lib/functions/notion";
@@ -5,13 +7,27 @@ import PageHeader from "@/app/components/PageHeader";
 import ArticleContainer from "@/app/components/articles/ArticleContainer";
 import ArticleList from "@/app/components/articles/ArticleList";
 
+import DevImage from "@/public/images/dev.png";
+import RetrospectImage from "@/public/images/retrospect.png";
+import StudyImage from "@/public/images/study.png";
+import ColumnImage from "@/public/images/column.png";
+import LifeImage from "@/public/images/life.png";
+
+const importImage: { [key in ArticleCategoryKeywords]: StaticImport } = {
+    dev: DevImage,
+    retrospect: RetrospectImage,
+    study: StudyImage,
+    column: ColumnImage,
+    life: LifeImage,
+};
+
 export default async function Category({ params }: { params: { category: ArticleCategoryKeywords } }) {
     const response = await fetchArticle({ category: params.category });
     const articleData = new ArticleResponse(response.items, response.nextCursor);
 
     return (
         <>
-            <PageHeader imageUrl={`/images/${params.category}.png`}>
+            <PageHeader imageSrc={importImage[params.category]}>
                 <div className="w-full my-4 absolute bottom-0 left-0 z-[1]">
                     <h1 className="text-center text-dark">{ArticleCategory[params.category]}</h1>
                 </div>
