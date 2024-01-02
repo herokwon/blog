33e3b8c2f1e8eb9467f108f606225d27b37ca478
herokwon <wooyng825@naver.com>
2024-01-02 14:11:01 +0900

    Style : bookmark code format

--- app/components/contents/Bookmark.tsx
+++ app/components/contents/Bookmark.tsx
@@ -14,18 +14,15 @@ export default async function Bookmark({ block }: { block: BookmarkBlockObjectRe
             href={pageUrl}>
             <div className={`article-content--bookmark-info ${metadata.imageUrl ? "w-2/3 md:w-3/4" : "w-full"} `}>
                 {metadata.title ?
-                    <h4 className="text-[1.2rem] line-clamp-1">{metadata.title}</h4> :
-                    null}
+                    <h4 className="text-[1.2rem] line-clamp-1">{metadata.title}</h4> : null}
                 {metadata.description ?
-                    <p className="text-[0.8rem] opacity-off line-clamp-2">{metadata.description}</p> :
-                    null}
+                    <p className="text-[0.8rem] opacity-off line-clamp-2">{metadata.description}</p> : null}
                 <div className="w-full flex items-center">
                     <i className={`${metadata.faviconUrl ? "w-16 h-16 bg-[length:16px_16px]" : "w-0 h-0"} mr-2 my-auto block bg-center bg-no-repeat`}
                         style={{
                             backgroundImage:
                                 metadata.faviconUrl ?
-                                    `url(${metadata.faviconUrl})` :
-                                    undefined
+                                    `url(${metadata.faviconUrl})` : undefined
                         }} />
                     <span className="text-[0.9rem] line-clamp-1">{pageUrl}</span>
                 </div>
@@ -34,8 +31,7 @@ export default async function Bookmark({ block }: { block: BookmarkBlockObjectRe
                 style={{
                     backgroundImage:
                         metadata.imageUrl ?
-                            `url(${metadata.imageUrl})` :
-                            undefined
+                            `url(${metadata.imageUrl})` : undefined
                 }} />
         </Link>
     );
