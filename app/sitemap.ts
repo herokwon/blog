56bf01221ba53c13eb26cbbcab30dd99464cef6a
herokwon <wooyng825@naver.com>
2024-01-02 20:13:58 +0900

    Refactor : union sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,37 @@
 import { MetadataRoute } from "next";
 
+import { BASE_URL } from "@/app/lib/data/constants";
 import { navItem } from "./lib/data/navItem";
-import { BASE_URL } from "./lib/data/constants";
+import { fetchArticle } from "@/app/lib/databases";
+import { ArticleResponse } from "@/app/types/notion";
+import { extractArticleProperties } from "@/app/lib/functions/notion";
 
-export default function Sitemap(): MetadataRoute.Sitemap {
-    return navItem.map((item) => ({
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    // page-sitemaps
+    const pageSitemap: MetadataRoute.Sitemap = navItem.map((item) => ({
         url: `${BASE_URL}${item.path}`,
         lastModified: new Date(),
-        changeFrequency:
-            item.path.includes("/posts") ?
-                'daily' : 'weekly',
+        changeFrequency: item.path.includes("https://") ? 'daily' : 'weekly',
     }));
+
+    const articleData = new ArticleResponse([], null);
+
+    do {
+        const response = await fetchArticle({ startCursor: articleData.nextCursor ?? undefined });
+        articleData.items.push(...response.items);
+        articleData.nextCursor = response.nextCursor;
+    } while (articleData.nextCursor);
+
+    // article-sitemap
+    const articleSitemap: MetadataRoute.Sitemap = articleData.items.map((article) => {
+        const properties = extractArticleProperties(article.properties);
+
+        return {
+            url: `${BASE_URL}/posts/${properties.Category}/${encodeURIComponent(properties.Title ?? "")}`,
+            lastModified: properties.Date ? new Date(properties.Date) : new Date(),
+            changeFrequency: 'daily',
+        };
+    });
+
+    return [...pageSitemap, ...articleSitemap];
 };
