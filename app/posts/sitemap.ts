757662fb202fbf2e05b98f1b4f900be0dfafa83c
herokwon <wooyng825@naver.com>
2024-01-02 15:37:48 +0900

    Feat : Add Sitemap (pages & articles)

---
+++ app/posts/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+import { BASE_URL } from "@/app/lib/data/constants";
+import { fetchArticle } from "@/app/lib/databases";
+import { extractArticleProperties } from "@/app/lib/functions/notion";
+import { ArticleCategory, ArticleCategoryKeywords, ArticleResponse } from "@/app/types/notion";
+
+export const generateSitemaps = () => {
+    return Object.keys(ArticleCategory).map((category: ArticleCategoryKeywords) => ({
+        id: category
+    }));
+};
+
+export default async function sitemap({ id }: { id: ArticleCategoryKeywords }): Promise<MetadataRoute.Sitemap> {
+    const articleData = new ArticleResponse([], null);
+
+    do {
+        const response = await fetchArticle({ category: id, startCursor: articleData.nextCursor ?? undefined });
+        articleData.items.push(...response.items);
+        articleData.nextCursor = response.nextCursor;
+    } while (articleData.nextCursor);
+
+    return articleData.items.map((article) => {
+        const properties = extractArticleProperties(article.properties);
+
+        return {
+            url: `${BASE_URL}/${id}/${encodeURIComponent(properties.Title ?? "")}`,
+            lastModified: properties.Date ? new Date(properties.Date) : new Date(),
+            changeFrequency: 'daily',
+        };
+    });
+};
