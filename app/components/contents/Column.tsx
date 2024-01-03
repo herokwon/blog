a2bc7b52fe7c15b16336c606dc0de4b7443d3365
herokwon <wooyng825@naver.com>
2024-01-03 18:47:19 +0900

    Refactor : move column contents

--- app/components/contents/Column.tsx
+++ app/components/contents/Column.tsx
@@ -1,13 +1,16 @@
 import { ColumnBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 import { fetchBlocks } from "@/app/lib/databases";
-import Block from "../Block";
+import Block from "./Block";
 
 export default async function Column({ block }: { block: ColumnBlockObjectResponse }) {
     const children = block.has_children ? await fetchBlocks(block.id) : null;
 
     return (
-        <div className="article-content--column">
+        <div className="article-content--column" style={{
+            gridTemplateColumns: "minmax(0, 1fr)",
+            gridTemplateRows: `repeat(${children?.items.length}, minmax(0, 1fr))`
+        }}>
             {children?.items.map((item, index) =>
                 <Block key={index} block={item} blocks={children?.items} index={index} />)}
         </div>
--- app/components/contents/ColumnList.tsx
+++ app/components/contents/ColumnList.tsx
@@ -1,13 +1,13 @@
 import { ColumnListBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 import { fetchBlocks } from "@/app/lib/databases";
-import Block from "../Block";
+import Block from "./Block";
 
 export default async function ColumnList({ block }: { block: ColumnListBlockObjectResponse }) {
     const children = block.has_children ? await fetchBlocks(block.id) : null;
 
     return (
-        <div className="article-content--column-list">
+        <div className="article-content--column-list" style={{ gridTemplateColumns: `repeat(${children?.items.length}, minmax(0, 1fr))` }}>
             {children?.items.map((item, index) =>
                 <Block key={index} block={item} blocks={children?.items} index={index} />)}
         </div>
