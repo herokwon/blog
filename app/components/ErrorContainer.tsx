bbf75779a107fd7a7938d902cb13f711c9986d7f
herokwon <wooyng825@naver.com>
2024-01-05 03:02:34 +0900

    Design : Update Errors UI

--- app/components/ErrorContainer.tsx
+++ app/components/ErrorContainer.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { RotateCcw } from "lucide-react";
+import { ArrowUpLeft, RotateCcw } from "lucide-react";
 
 import Button from "./Button";
+import { useRouter } from "next/navigation";
 
 export interface ErrorProps {
     title?: string;
@@ -13,22 +14,27 @@ export interface ErrorProps {
 };
 
 export default function ErrorContainer({ title, error, reset }: ErrorProps) {
+    const router = useRouter();
+
     return (
-        <section className="w-full min-h-screen flex flex-col justify-center items-center">
-            <h1>Error</h1>
-            {title ?
-                <h2>{title}</h2> : null}
-            <div className="my-block">
-                <h3>오류 내용</h3>
-                <p className="px-4 py-2 my-2 rounded-lg text-center bg-light-secondary dark:bg-dark-secondary">{error.message}</p>
-            </div>
-            <div className="my-block">
+        <section className="w-full max-w-screen-lg min-h-screen mx-auto flex flex-col justify-center items-center">
+            <h1 className="w-full text-center">{title ? `Error : ${title}` : "Error"}</h1>
+            <p className="w-full text-center">불편을 드려 죄송합니다.</p>
+            <p className="w-1/2 min-w-[300px] px-8 py-4 my-block rounded-lg text-center bg-light-secondary dark:bg-dark-secondary">{`오류 내용 : ${error.message}`}</p>
+            <div className="w-full my-block flex justify-center items-center">
                 <Button
                     innerType="text"
-                    className="bg-light-tertiary dark:bg-dark-tertiary"
+                    className="px-4 bg-light-secondary dark:bg-dark-secondary"
                     onClick={reset}>
-                    <span className="mx-2">다시 시도하기</span>
+                    <span className="mr-2">다시 시도하기</span>
                     <RotateCcw size={16} />
+                </Button>
+                <Button
+                    innerType="text"
+                    className="px-4 bg-light-secondary dark:bg-dark-secondary"
+                    onClick={() => router.back()}>
+                    <span className="mr-2">뒤로 가기</span>
+                    <ArrowUpLeft size={16} />
                 </Button>
             </div>
         </section>
