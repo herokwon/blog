d423a91c046ef6d99ca1bec9ba45bf41759e076e
herokwon <wooyng825@naver.com>
2024-01-09 12:10:25 +0900

    Refactor : Toc

--- app/components/Toc.tsx
+++ app/components/Toc.tsx
@@ -5,14 +5,10 @@ import { List, X } from "lucide-react";
 import Link from "next/link";
 
 import { Headings } from "../types/notion";
-import { useToc } from "../hooks/useToc";
 import Button from "./Button";
 
-export default function Toc({ headings }: { headings: Headings[] }) {
-    const [activeId, setActiveId] = useState<string>("");
+export default function Toc({ activeId, headings }: { activeId: string; headings: Headings[] }) {
     const [isTocActive, setIsTocActive] = useState<boolean>(false);
-
-    useToc(headings, setActiveId);
 
     useEffect(() => {
         const rangeStart = window.innerHeight * 0.5;
@@ -31,8 +27,7 @@ export default function Toc({ headings }: { headings: Headings[] }) {
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [activeId]);
 
     return (
         <>
@@ -55,7 +50,7 @@ export default function Toc({ headings }: { headings: Headings[] }) {
             </div>
             <Button
                 innerType="icon"
-                className="article-toc--btn relative"
+                className="article-toc--btn"
                 onClick={() => setIsTocActive(true)}>
                 <List size={20} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
             </Button>
