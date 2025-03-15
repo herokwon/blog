f110881c5a641a50cb61a038f851d1715aa8ebea
herokwon <contact@herokwon.dev>
2025-03-15 23:49:22 +0900

    Feat (components): `ThemeButton` 컴포넌트 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import '../styles/globals.css';
 
+import { getTheme } from '@lib';
+
 import { NavigationBar } from '@layouts';
 
-export default function RootLayout({
+import { ThemeButton } from '@components';
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const theme = await getTheme();
+
   return (
-    <html lang="ko">
+    <html lang="ko" className={theme ?? 'light'}>
       <body>
         <header className="sticky top-0 left-0 w-full backdrop-blur-md">
           <NavigationBar />
+          <ThemeButton theme={theme} />
         </header>
         {children}
       </body>
