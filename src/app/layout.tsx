99fc35f80099790663564c9234b81037a68f5833
herokwon <contact@herokwon.dev>
2025-03-16 17:27:19 +0900

    Feat: 로고 이미지 하이퍼링크 링크 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 import '../styles/globals.css';
 
 import { getTheme } from '@lib';
 
 import { NavigationBar } from '@layouts';
 
-import { ThemeButton } from '@components';
+import { Logo, ThemeButton } from '@components';
 
 export default async function RootLayout({
   children,
@@ -17,7 +19,15 @@ export default async function RootLayout({
     <html lang="ko" className={theme ?? 'light'}>
       <body>
         <header className="sticky top-0 left-0 flex h-12 w-full px-4 backdrop-blur-md">
-          <div className="flex h-full flex-1 items-center justify-start"></div>
+          <div className="flex h-full flex-1 items-center justify-start">
+            <Link
+              href="/"
+              className="relative h-full w-40 *:absolute *:top-1/2 *:left-1/2 *:-translate-1/2 *:transition-opacity *:last:-z-1 not-hover:*:last:pointer-events-none not-hover:*:last:opacity-0"
+            >
+              <Logo onlyText />
+              <Logo />
+            </Link>
+          </div>
           <NavigationBar />
           <div className="flex h-full flex-1 items-center justify-end">
             <ThemeButton theme={theme} />
