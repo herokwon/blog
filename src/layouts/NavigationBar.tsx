d02855701adc7018cfe5c745aa8989ab76e6a7ef
herokwon <contact@herokwon.dev>
2025-03-17 16:41:01 +0900

    Design (components): `NavigationBar` 컴포넌트 반응형 스타일 추가

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -14,12 +14,12 @@ export default function NavigationBar() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex size-full flex-3 items-center justify-center gap-x-2">
+    <nav className="max-md:bg-secondary-light dark:max-md:bg-secondary-dark max-md:shadow-secondary-light dark:max-md:shadow-secondary-dark flex size-full items-center justify-center gap-x-2 gap-y-4 max-md:h-max max-md:max-w-75 max-md:flex-col max-md:rounded-md max-md:p-4 max-md:shadow">
       {NAV_ITEMS.map(({ path, title }) => (
         <Link
           key={`${title}${path}`}
           href={path}
-          className={`from-foreground-light to-foreground-light bg-underline-0.5 dark:from-foreground-dark dark:to-foreground-dark flex h-full items-center bg-linear-to-r px-2 transition-all duration-200 ${
+          className={`from-foreground-light to-foreground-light bg-underline-0.5 dark:from-foreground-dark dark:to-foreground-dark flex items-center bg-linear-to-r p-2 transition-all duration-200 ${
             pathname === path ? 'active font-bold' : ''
           }`}
         >
