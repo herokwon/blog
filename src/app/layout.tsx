d957d5837b01282f4bba014edbcdd6f43b76975c
herokwon <contact@herokwon.dev>
2025-03-19 23:27:59 +0900

    Chore: `Vercel` 웹 페이지 모니터링 도구 추가 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Link from 'next/link';
 
 import '../styles/globals.css';
@@ -39,6 +41,8 @@ export default async function RootLayout({
           </div>
         </header>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
