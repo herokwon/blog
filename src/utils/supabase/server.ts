88fde4531fa93f4321bf773b532b30f3f77f5d58
herokwon <contact@herokwon.dev>
2025-10-16 09:35:27 +0900

    fix(server): 오류 메시지에 에러 정보를 추가하여 쿠키 설정 실패 시 더 많은 정보 제공

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -17,8 +17,8 @@ export async function createClient() {
           cookiesToSet.forEach(({ name, value, options }) =>
             cookieStore.set(name, value, options),
           );
-        } catch {
-          console.error('Failed to set cookies on server side');
+        } catch (error) {
+          console.error('Failed to set cookies on server side: ', error);
         }
       },
     },
