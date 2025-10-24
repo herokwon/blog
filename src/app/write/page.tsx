bcc1a22cb8fc8b3dba702858464d0dfd01d59ee8
herokwon <contact@herokwon.dev>
2025-10-24 17:44:03 +0900

    refactor(write): 게시글 작성 페이지 재구성

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,27 +1,15 @@
-import { EditorShell } from '@/features/Editor';
-import { BookPlus } from 'lucide-react';
+'use client';
+
+import { PostForm, PublishButton, useCreatePost } from '@/features/Post';
 
 export default function Write() {
+  const { create } = useCreatePost();
+
   return (
-    <section className="mx-auto grid size-full max-w-5xl grid-rows-[min-content_minmax(0,_1fr)] gap-y-4 py-12">
-      <div>
-        <button
-          type="button"
-          className="mr-0 ml-auto flex cursor-pointer items-center gap-x-2 rounded bg-indigo-500 px-3 py-1 font-semibold text-white hover:bg-indigo-600 active:bg-indigo-700"
-        >
-          <BookPlus size={16} />
-          발행
-        </button>
-      </div>
-      <article className="flex flex-col gap-y-4">
-        <input
-          id="title"
-          name="title"
-          placeholder="제목"
-          className="w-full flex-shrink-0 rounded px-4 py-3 font-bold ring-1 ring-slate-200 outline-none"
-        />
-        <EditorShell className="min-h-0 flex-1" />
-      </article>
+    <section className="mx-auto size-full max-w-5xl py-12">
+      <PostForm onSubmit={create}>
+        <PublishButton />
+      </PostForm>
     </section>
   );
 }
