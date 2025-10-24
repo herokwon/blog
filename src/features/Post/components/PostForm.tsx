b078c91f4f369be502478ee360344b80704f01d8
herokwon <contact@herokwon.dev>
2025-10-24 17:39:51 +0900

    feat(post): `PostForm` 컴포넌트 구현

---
+++ src/features/Post/components/PostForm.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { EditorShell } from '@/features/Editor';
+
+import type { Post, PostRequest } from '../types';
+
+type PostFormProps = Omit<React.ComponentPropsWithRef<'form'>, 'onSubmit'> & {
+  initialData?: PostRequest;
+  isLoading?: boolean;
+  onChangeData?: (data: PostRequest) => void;
+  onSubmit: (data: PostRequest) => Promise<Post | null>;
+};
+
+export const PostForm = ({
+  children,
+  initialData = {
+    title: '',
+    content: '',
+  },
+  onChangeData,
+  onSubmit,
+  ...props
+}: PostFormProps) => {
+  const [post, setPost] = useState<PostRequest>({
+    ...initialData,
+  });
+
+  const handleChange = (data: PostRequest) => {
+    setPost(data);
+    onChangeData?.(data);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await onSubmit(post);
+  };
+
+  useEffect(() => {
+    setPost({ ...initialData });
+  }, [initialData]);
+
+  return (
+    <form
+      {...props}
+      onSubmit={handleSubmit}
+      className={`flex size-full grid-rows-[min-content_minmax(0,_1fr)] flex-col gap-y-4 ${props.className ?? ''}`}
+    >
+      {children && <div className="mr-0 ml-auto">{children}</div>}
+      <input
+        id="title"
+        name="title"
+        value={post.title}
+        placeholder="제목"
+        className="w-full flex-shrink-0 rounded px-4 py-3 font-bold ring-1 ring-slate-200 outline-none"
+        onChange={e => handleChange({ ...post, title: e.target.value })}
+      />
+      <EditorShell
+        className="min-h-0 flex-1"
+        value={post.content}
+        onChangeValue={newContent =>
+          handleChange({ ...post, content: newContent })
+        }
+      />
+    </form>
+  );
+};
