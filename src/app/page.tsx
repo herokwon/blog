01f75cc8cb8679dc2c35ebad4a7b74ad57b14e0c
herokwon <contact@herokwon.dev>
2025-11-27 18:32:26 +0900

    feat: 홈 페이지 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return;
+import { getPosts, isError, PostList } from '@/features/Post';
+
+export default async function Home() {
+  const response = await getPosts(5);
+
+  if (isError(response)) throw new Error(response.error);
+  const { data: posts } = response;
+
+  return <PostList posts={posts} />;
 }
