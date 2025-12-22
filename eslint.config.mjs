a616d0fa71e69b16b36c6fec07687d338c169a35
HeroKwon <contact@herokwon.dev>
2025-12-22 20:26:15 +0900

    fix: Cloudflare Workers 배포 페이지 접속 오류 (#90)
    * fix: `.npmrc` 파일 복구
    
    * fix(wrangler): `workers_dev` 속성 추가
    
    * chore: pnpm lockfile 재생성
    
    * chore(package): `gen-types` 스크립트에 포맷팅 추가
    
    * chore(deps): `next` 및 관련 의존성 버전 다운그레이드 (v16 → v15)
    
    * fix(next): `next@v15`에 호환 가능하도록 설정 수정
    
    * fix(eslint): `next@v15`에 호환 가능하도록 설정 수정
    
    * fix(tsconfig): JSX 설정 수정('react-jsx' → 'preserve')
    
    * remove: `.npmrc` 파일 제거

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,55 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTs from 'eslint-config-next/typescript';
-import prettier from 'eslint-config-prettier/flat';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  prettier,
-  {
-    rules: {
-      'no-restricted-imports': [
-        'error',
-        {
-          patterns: ['@/features/*/*'],
-        },
-      ],
-    },
-  },
-  globalIgnores([
-    '.next/**',
-    'out/**',
-    'build/**',
-    'dist/**',
-    'next-env.d.ts',
-    'node_modules/**',
-    '.wrangler/**',
-    '.open-next/**',
-    'coverage/**',
-    'public/**',
-  ]),
-]);
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+];
 
 export default eslintConfig;
+
+// next.js v16 uses format
+// import nextVitals from 'eslint-config-next/core-web-vitals';
+// import nextTs from 'eslint-config-next/typescript';
+// import prettier from 'eslint-config-prettier/flat';
+// import { defineConfig, globalIgnores } from 'eslint/config';
+
+// const eslintConfig = defineConfig([
+//   ...nextVitals,
+//   ...nextTs,
+//   prettier,
+//   {
+//     rules: {
+//       'no-restricted-imports': [
+//         'error',
+//         {
+//           patterns: ['@/features/*/*'],
+//         },
+//       ],
+//     },
+//   },
+//   globalIgnores([
+//     '.next/**',
+//     'out/**',
+//     'build/**',
+//     'dist/**',
+//     'next-env.d.ts',
+//     'node_modules/**',
+//     '.wrangler/**',
+//     '.open-next/**',
+//     'coverage/**',
+//     'public/**',
+//   ]),
+// ]);
+
+// export default eslintConfig;
