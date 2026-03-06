f38e5f8149d7216c4c4da06fad40f589c3778290
herokwon <contact@herokwon.dev>
2026-03-07 04:04:13 +0900

    ✨ feat(posts): Implement posts list

---
+++ src/routes/posts/+page.server.ts
@@ -0,0 +1,15 @@
+import type { ApiResponse } from '$lib/types/api';
+import type { Post } from '$lib/types/post';
+
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ fetch }) => {
+  const res = await fetch('/api/posts');
+  const response: ApiResponse<Post[]> = await res.json();
+
+  if (!response.success) {
+    return { posts: [] as Post[], loadError: response.error.message };
+  }
+
+  return { posts: response.data };
+};
