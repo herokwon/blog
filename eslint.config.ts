aae9c50781d9aefd5df6363c690da6d18479c08b
HeroKwon <contact@herokwon.dev>
2026-03-10 23:33:39 +0900

    refactor: Improve the static analysis tooling infrastructure (with. `ESLint`, `Prettier`, etc.) (#32)
    * ⚙️ chore(husky): Remove script sourcing from `prepare-commit-msg` hook
    
    * ⚙️ chore(package): Update `prepare` script to use husky for commit hooks
    
    * ⚙️ chore(package): Update `lint` script to enforce no warnings
    
    * ⚙️ chore(package): Add `lint:fix` script to automatically fix linting issues
    
    * ⚙️ chore(husky): Add `pre-commit` hook to enforce linting rules
    
    * ⚙️ chore(prettier): Reorder plugins for consistency
    
    * ⚙️ chore(vscode): Update settings for `Prettier` and `ESLint` integration
    
    * ⚙️ chore(editorconfig): Add initial configuration for code formatting
    
    * ⚙️ chore(husky): Add emoji prefix for release commit messages
    
    * ⚙️ chore(svelte): Migrate config to TypeScript
    
    * ⚙️ chore(eslint): Migrate config to TypeScript
    
    * ⚙️ chore(workflows/ci): Simplify job names

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,7 +9,7 @@ import globals from 'globals';
 import path from 'node:path';
 import ts from 'typescript-eslint';
 
-import svelteConfig from './svelte.config.js';
+import svelteConfig from './svelte.config.ts';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
